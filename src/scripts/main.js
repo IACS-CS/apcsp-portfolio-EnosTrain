65ebc2ef75e8606285b1d0468712f3da595257bf
EnosTrain <xavier.enos@innovationcharter.org>
2024-10-01 15:00:26 +0000

    Up To Step Three!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,8 @@ if (typeof window !== "undefined") {
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
 }
+import "@picocss/pico/css/pico.min.css"; 
+<main class ="container"
+>
+
+</main>
